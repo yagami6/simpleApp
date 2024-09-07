4b201f6547aadd0b65cdede5fb08a5a68bc32eda
Mona Lisa <sushant@gmail.com>
2024-09-07 23:30:42 +0530

    added first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://backend:4000/')
+      .then(response => response.json())
+      .then(data => setMessage(data.Hello));
+  }, []);
+
+  return (
+    <div>
+      <header>
+        <h1>Hello World from React!</h1>
+        <h2>Message from API: {message}</h2>
+      </header>
+    </div>
+  );
+};
+
+export default App;
